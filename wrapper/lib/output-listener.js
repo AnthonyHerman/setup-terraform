1fe6a53d3ce309ca05881a12d42f2988d39016a5
anthonyherman <anthonyherman>
2021-06-11 17:17:23 +0000

    test

--- wrapper/lib/output-listener.js
+++ wrapper/lib/output-listener.js
@@ -27,8 +27,9 @@ class OutputListener {
   }
 
   get contents () {
-    x := this._buff.map(chunk => chunk.toString()).join('');
-    return x;
+    return this._buff.map(chunk => chunk.toString()).join('');
+    //x := this._buff.map(chunk => chunk.toString()).join('');
+    //return x;
   }
 }
 
